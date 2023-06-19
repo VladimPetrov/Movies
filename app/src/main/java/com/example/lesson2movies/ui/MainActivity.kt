b16bd2129b7a94bb3345693f4d18ad6293c0da03
VladimPetrov <vl.pva@yandex.ru>
2023-06-19 10:31:55 +0300

    add binding

--- app/src/main/java/com/example/lesson2movies/ui/MainActivity.kt
+++ app/src/main/java/com/example/lesson2movies/ui/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.lesson2movies
+package com.example.lesson2movies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lesson2movies.R
+import com.example.lesson2movies.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment.newInstance())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/lesson2movies/ui/main/MainFragment.kt
@@ -0,0 +1,12 @@
+package com.example.lesson2movies.ui.main
+
+import androidx.fragment.app.Fragment
+
+class MainFragment:Fragment() {
+
+
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+}
